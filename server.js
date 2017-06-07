f09bf20f7902ffd61d70d90fbee5d2ba90849f51
Bao Phung <baophung@BaoPhung.local>
2017-06-07 01:01:08 -0700

    Completed server file, but need to style

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var path = require('path');
 var express = require('express');
 var handle = require('express-handlebars');
-var final = express();
 var letterNumber = require('./letterNumber');
+var final = express();
 var port = process.env.PORT || 3000;
 
 final.engine('handlebars', handle({defaultLayout: 'main'}));
@@ -12,72 +12,72 @@ final.use(express.static(path.join(__dirname, 'public')));
 final.get('/', function(request, response, next){
 	response.render('page', {
 		letters: letterNumber,
-		single: false
+		many: true
 	});
 });
 
 final.get('/letters/:index', function(request, response, next){
 	var index = request.params.index;
 	switch(index){
-		case 'a': case 'A':
-			index = 0; break;
-		case 'b':case 'B':
-			index = 1; break;
-		case 'c':case 'C':	
-			index = 2; break;
-		case 'd':case 'D':
-			index = 3; break;
-		case 'e':case 'E':
-			index = 4;  break;
-		case 'f':case 'F':
-			index = 5; break;
-		case 'g':case 'G':
-			index = 6; break;
-		case 'h':case 'H':
-			index = 7; break;
-		case 'i':case 'I':
-			index = 8; break;
-		case 'j':case 'J':
-			index = 9; break;
-		case 'k':case 'K':
-			index = 10; break;
-		case 'l':case 'L':
-			index = 11; break;
-		case 'm':case 'M':
-			index = 12; break;
-		case 'n':case 'N':
-			index = 13; break;
-		case 'o':case 'O':
-			index = 14; break;
-		case 'p':case 'P':
-			index = 15; break;
-		case 'q':case 'Q':
-			index = 16; break;
-		case 'r':case 'R':
-			index = 17; break;
-		case 's':case 'S':
-			index = 18; break;
-		case 't':case 'T':
-			index = 19; break;
-		case 'u':case 'U':
-			index = 20; break;
-		case 'v':case 'V':
-			index = 21; break;
-		case 'w':case 'W':
-			index = 22; break;
-		case 'x':case 'X':
-			index = 23; break;
-		case 'y':case 'Y':
-			index = 24; break;
-		case 'z':case 'Z':
-			index = 25; break;
+		case 'a': index = 0; break;
+		case 'A': index = 0; break;
+		case 'b': index = 1; break;
+		case 'B': index = 1; break;
+		case 'c': index = 2; break;
+		case 'C': index = 2; break;
+		case 'd': index = 3; break;
+		case 'D': index = 3; break;
+		case 'e': index = 4; break;
+		case 'E': index = 4; break;
+		case 'f': index = 5; break;
+		case 'F': index = 5; break;
+		case 'g': index = 6; break;
+		case 'G': index = 6; break;
+		case 'h': index = 7; break;
+		case 'H': index = 7; break;
+		case 'i': index = 8; break;
+		case 'I': index = 8; break;
+		case 'j': index = 9; break;
+		case 'J': index = 9; break;
+		case 'k': index = 10; break;
+		case 'K': index = 10; break;
+		case 'l': index = 11; break;
+		case 'L': index = 11; break;
+		case 'm': index = 12; break;
+		case 'M': index = 12; break;
+		case 'n': index = 13; break;
+		case 'N': index = 13; break;
+		case 'o': index = 14; break;
+		case 'O': index = 14; break;
+		case 'p': index = 15; break;
+		case 'P': index = 15; break;
+		case 'q': index = 16; break;
+		case 'Q': index = 16; break;
+		case 'r': index = 17; break;
+		case 'R': index = 17; break;
+		case 's': index = 18; break;
+		case 'S': index = 18; break;
+		case 't': index = 19; break;
+		case 'T': index = 19; break;
+		case 'u': index = 20; break;
+		case 'U': index = 20; break;
+		case 'v': index = 21; break;
+		case 'V': index = 21; break;
+		case 'w': index = 22; break;
+		case 'W': index = 22; break;
+		case 'x': index = 23; break;
+		case 'X': index = 23; break;
+		case 'y': index = 24; break;
+		case 'Y': index = 24; break;
+		case 'z': index = 25; break;
+		case 'Z': index = 25; break;
 	}
+	console.log(index);
 	var specificLetter = letterNumber[index];
 	if(specificLetter){
 		response.render('page', {
-			alpha: specificLetter.letter,
-			description: specificLetter.description,
-			single: true
+			letters: [specificLetter],
+			many: false
 		});
 	}
 	else{
