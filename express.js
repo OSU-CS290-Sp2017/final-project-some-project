7f9b30aaf0c3b635e791763962adf0dd9bacfa50
Bao Phung <baophung@BaoPhung.local>
2017-06-09 06:20:37 -0700

    Updated Express server

--- express.js
+++ express.js
@@ -3,18 +3,28 @@ var express = require('express');
 var handle = require('express-handlebars');
 var letterNumber = require('./letterNumber');
 var final = express();
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
 final.engine('handlebars', handle({defaultLayout: 'main'}));
 final.set('view engine', 'handlebars');
-final.use(express.static(path.join(__dirname, 'public')));
+final.use(express.static(path.join(__dirname, 'public'))); /*statically retrieves style.css and index.js*/
+final.use(express.static(path.join(__dirname, '/views/images'))); /*statically retrieves all images from /views/images*/
+final.use(bodyParser.json());
 
 final.get('/', function(request, response, next){
 	response.render('page', {
 		letters: letterNumber,
+		letter: "Alphabets",
 		alphabet: "Alphabets",
-		many: true,
-		single: false
+	});
+});
+
+final.get('/index.html', function(request, response, next){
+	response.render('page', {
+		letters: letterNumber, 
+		letter: "Alphabets",
+		alphabet: "Alphabets"
 	});
 });
 
@@ -22,73 +32,220 @@ final.get('/letters/:index', function(request, response, next){
 	var index = request.params.index;
 	var alpha = index;
 	switch(index){
-		case 'a': index = 0; break;
-		case 'A': index = 0; break;
-		case 'b': index = 1; break;
-		case 'B': index = 1; break;
-		case 'c': index = 2; break;
-		case 'C': index = 2; break;
-		case 'd': index = 3; break;
-		case 'D': index = 3; break;
-		case 'e': index = 4; break;
-		case 'E': index = 4; break;
-		case 'f': index = 5; break;
-		case 'F': index = 5; break;
-		case 'g': index = 6; break;
-		case 'G': index = 6; break;
-		case 'h': index = 7; break;
-		case 'H': index = 7; break;
-		case 'i': index = 8; break;
-		case 'I': index = 8; break;
-		case 'j': index = 9; break;
-		case 'J': index = 9; break;
-		case 'k': index = 10; break;
-		case 'K': index = 10; break;
-		case 'l': index = 11; break;
-		case 'L': index = 11; break;
-		case 'm': index = 12; break;
-		case 'M': index = 12; break;
-		case 'n': index = 13; break;
-		case 'N': index = 13; break;
-		case 'o': index = 14; break;
-		case 'O': index = 14; break;
-		case 'p': index = 15; break;
-		case 'P': index = 15; break;
-		case 'q': index = 16; break;
-		case 'Q': index = 16; break;
-		case 'r': index = 17; break;
-		case 'R': index = 17; break;
-		case 's': index = 18; break;
-		case 'S': index = 18; break;
-		case 't': index = 19; break;
-		case 'T': index = 19; break;
-		case 'u': index = 20; break;
-		case 'U': index = 20; break;
-		case 'v': index = 21; break;
-		case 'V': index = 21; break;
-		case 'w': index = 22; break;
-		case 'W': index = 22; break;
-		case 'x': index = 23; break;
-		case 'X': index = 23; break;
-		case 'y': index = 24; break;
-		case 'Y': index = 24; break;
-		case 'z': index = 25; break;
-		case 'Z': index = 25; break;
+		case 'a': case 'A': index = 0; break;
+		case 'b': case 'B': index = 1; break;
+		case 'c': case 'C': index = 2; break;
+		case 'd': case 'D': index = 3; break;
+		case 'e': case 'E': index = 4; break;
+		case 'f': case 'F': index = 5; break;
+		case 'g': case 'G': index = 6; break;
+		case 'h': case 'H': index = 7; break;
+		case 'i': case 'I': index = 8; break;
+		case 'j': case 'J': index = 9; break;
+		case 'k': case 'K': index = 10; break;
+		case 'l': case 'L': index = 11; break;
+		case 'm': case 'M': index = 12; break;
+		case 'n': case 'N': index = 13; break;
+		case 'o': case 'O': index = 14; break;
+		case 'p': case 'P': index = 15; break;
+		case 'q': case 'Q': index = 16; break;
+		case 'r': case 'R': index = 17; break;
+		case 's': case 'S': index = 18; break;
+		case 't': case 'T': index = 19; break;
+		case 'u': case 'U': index = 20; break;
+		case 'v': case 'V': index = 21; break;
+		case 'w': case 'W': index = 22; break;
+		case 'x': case 'X': index = 23; break;
+		case 'y': case 'Y': index = 24; break;
+		case 'z': case 'Z': index = 25; break;
 	}
-	var specificLetter = letterNumber[index];
-	if(specificLetter){
-		response.render('page', {
+
+	if(alpha == 'A' || alpha == 'a'){
+		response.render('A', {
 			alphabet: alpha.toUpperCase(),
-			letter: alpha.toUpperCase(),
-			letters: specificLetter.letter,
-			description: specificLetter.description,
-			many: false,
-			single: true
+			letter: alpha.toUpperCase()
 		});
 	}
+
+	else if(alpha == 'B' || alpha == 'b'){
+		response.render('B', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'C' || alpha == 'c'){
+		response.render('C', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'D' || alpha == 'd'){
+		response.render('D', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'E' || alpha == 'e'){
+		response.render('E', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'F' || alpha == 'f'){
+		response.render('F', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'G' || alpha == 'g'){
+		response.render('G', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'H' || alpha == 'h'){
+		response.render('H', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'I' || alpha == 'i'){
+		response.render('I', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'J' || alpha == 'j'){
+		response.render('J', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'K' || alpha == 'k'){
+		response.render('K', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'L' || alpha == 'l'){
+		response.render('L', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'M' || alpha == 'm'){
+		response.render('M', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'N' || alpha == 'n'){
+		response.render('N', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'O' || alpha == 'o'){
+		response.render('O', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'P' || alpha == 'p'){
+		response.render('P', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'Q' || alpha == 'q'){
+		response.render('Q', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'R' || alpha == 'r'){
+		response.render('R', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'S' || alpha == 's'){
+		response.render('S', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'T' || alpha == 't'){
+		response.render('T', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'U' || alpha == 'u'){
+		response.render('U', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'V' || alpha == 'v'){
+		response.render('V', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'W' || alpha == 'w'){
+		response.render('W', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'X' || alpha == 'x'){
+		response.render('X', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'Y' || alpha == 'y'){
+		response.render('Y', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
+	else if(alpha == 'Z' || alpha == 'z'){
+		response.render('Z', {
+			alphabet: alpha.toUpperCase(),
+			letter: alpha.toUpperCase()
+		});
+	}
+
 	else{
 		next();
 	}
+
 });
 
 final.get('*', function(request, response, next){
